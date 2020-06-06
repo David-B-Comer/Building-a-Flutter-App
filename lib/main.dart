7d337417014b584a4e15eb09144b9023fbf7d69b
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-06-06 14:08:25 -0400

    added word pair suggestions

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,8 @@ class MyApp extends StatelessWidget {
 
 //lib/main.dart (RandomWordsState)
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
