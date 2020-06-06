b44e42730bf8c52e2c88b889ab365262060b2b7b
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-06-06 14:14:00 -0400

    added build suggestions

--- lib/main.dart
+++ lib/main.dart
@@ -40,3 +40,18 @@ class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => RandomWordsState();
 }
+
+//lib/main.dart (_buildSuggestions)
+Widget _buildSuggestions() {
+  return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return Divider(); /*2*/
+
+        final index = i ~/ 2; /*3*/
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        return _buildRow(_suggestions[index]);
+      });
+}
